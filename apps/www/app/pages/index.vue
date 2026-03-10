537e0445b678a3cc2d36651f7f727dfa81de3702
psycarlo <psycarlo1@gmail.com>
2026-03-10 00:24:23 +0000

    feat: ship

--- apps/www/app/pages/index.vue
+++ apps/www/app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <main class="container-narrow">
-    <div class="flex flex-col gap-2 items-center pt-100">
+  <main class="container-narrow relative">
+    <ClientOnly> <ParticleSwarm class="top-20!" /> </ClientOnly>
+    <div class="relative z-10 flex flex-col gap-2 items-center pt-100">
       <h1 class="font-bold text-5xl">Charting the AI future</h1>
       <p class="text-lg text-muted-foreground">
         We develop and deploy applied AI solutions to bring our partners to the
